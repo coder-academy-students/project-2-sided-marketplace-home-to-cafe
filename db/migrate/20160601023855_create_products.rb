327e0737ef67f8ec334c328b87b137066cd0ff32
Marshall Gray <marshallgray1992@gmail.com>
2016-06-01 15:27:10 +1000

    remove drop table

--- db/migrate/20160601023855_create_products.rb
+++ db/migrate/20160601023855_create_products.rb
@@ -1,7 +1,5 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
-    drop_table :products
-
     create_table :products do |t|
       t.references :user, foreign_key: true
       t.string :cake_type
