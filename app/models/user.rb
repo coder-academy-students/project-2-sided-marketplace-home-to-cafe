07a356507ef333a91b47278aa5910717af3c5d1c
Marshall Gray <marshallgray1992@gmail.com>
2016-06-02 12:26:07 +1000

    bootstrap added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :reviews, :class_name => 'Review', :foreign_key => 'reviewee'
   has_many :reviewers, :class_name => 'Review', :foreign_key => 'reviewer'
   has_many :products
+  has_one :cafe_owner_profiles
+  has_one :home_baker_profiles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
